c0771418af29fc1b35493cc0ae2b81ade8b3b49a
haryobamy <haryobamy.badmus@gmail.com>
2020-12-19 16:01:05 +0100

    homerunner

--- src/components/Verification.js
+++ src/components/Verification.js
@@ -47,7 +47,7 @@ class Verification  extends Component {
 
                         <div id="wrapper">
   <div id="dialog">
-    <h6>Please enter the 4-digit verification code we sent via SMS:</h6>
+    <h6>Please enter the 4-digit verification code we sent via SMS:</h6git >
     <div id="form">
       <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" />
       <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" /><input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" /><input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" />
